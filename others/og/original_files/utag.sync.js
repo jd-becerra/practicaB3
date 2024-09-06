6d4c1d0013c03efb3eb901dd3c3bf32aebe3b16b
jd-becerra <jd-becerra>
2024-09-06 12:03:42 -0600

    others folder rm

--- others/og/original_files/utag.sync.js
+++
@@ -1,1 +0,0 @@
-//tealium universal tag - utag.sync ut4.0.202408120955, Copyright 2024 Tealium.com Inc. All Rights Reserved.
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,34 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // custom colors
+      colors: {
+        black: '#121212',
+      },
+    },
+
+    // Color overrides
+    colors: {
+      red: {
+        300: '#EBB5B5',
+        600: '#CC0000',
+        950: '#720000',
+      },
+      white: '#FAFAFA',
+      gray: {
+        100: '#EEECEC',
+        200: '#EEEEEE',
+        300: '#C2C2C2',
+        600: '#595858',
+        800: '#1B242B',
+        950: '#121212',
+      },
+      blue: {
+        300: '#60A9FF',
+        600: '#0F57AC',
+      },
+    }
   },
   plugins: [],
 }
