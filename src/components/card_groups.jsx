8c16c39797da502ac9b0838ec72c1462983f1ff3
jd-becerra <jd-becerra>
2024-09-08 00:16:50 -0600

    Añadido Footer

--- src/components/card_groups.jsx
+++ src/components/card_groups.jsx
@@ -3,29 +3,29 @@ import { CardFrontPage, CardFrontPageSmall, CardType1, CardType2 } from './cards
 
 function FrontPage() {
   return (
-      <div className='grid grid-cols-3 grid-rows-4 gap-4'>
-        <div className='col-span-3 row-span-1 p-x-12 bg-gray-100 flex justify-center items-center border-4 border-white'>
-          <img
-            className='drop-shadow-xl mx-auto scale-90'
-            src='../img/banners/caliente_horizontal.gif'
+    <div className='grid grid-cols-3 grid-rows-4 gap-4'>
+      <div className='col-span-3 row-span-1 p-x-12 bg-gray-100 flex justify-center items-center bg-white'>
+        <img
+          className='drop-shadow-xl mx-auto scale-90 cursor-pointer'
+          src='../img/banners/caliente_horizontal.gif'
+        />
+      </div>
+      <div className='col-span-3 row-span-2 grid grid-cols-4 gap-8'>
+        <div className='col-span-3 container h-full w-full'>
+          <CardFrontPage {...front_page_card_data} />
+        </div>
+        <div className='col-span-1 container border-4 border-white'>
+          <img 
+            className='object-cover h-full w-full drop-shadow-xl cursor-pointer'
+            src='../img/banners/codere_vertical.gif' 
+            alt='banner' 
           />
         </div>
-        <div className='col-span-3 row-span-2 grid grid-cols-4 gap-8'>
-          <div className='col-span-3 container border-4 border-white h-full w-full'>
-            <CardFrontPage {...front_page_card_data} />
-          </div>
-          <div className='col-span-1 container border-4 border-white'>
-            <img 
-              className='object-cover h-full w-full drop-shadow-xl'
-              src='../img/banners/codere_vertical.gif' 
-              alt='banner' 
-            />
-          </div>
-        </div>
-        {c_fp_data.map((card_data, index) => (
-          <CardFrontPageSmall key={index} {...card_data} />
-        ))}
       </div>
+      {c_fp_data.map((card_data, index) => (
+        <CardFrontPageSmall key={index} {...card_data} />
+      ))}
+    </div>
   )
 }
 
--- src/components/separators.jsx
+++ src/components/separators.jsx
@@ -5,18 +5,18 @@ import { c_separator_data } from '../data/cards_data.jsx'
 
 function Separator1() {
   return (
-    <div className='container mx-auto inline-flex border-y-4 border-y-gray-200 py-8 my-16 overflow-hidden relative'>
+    <div className='container mx-auto inline-flex border-y-4 border-y-gray-300 py-2 my-16 overflow-hidden relative'>
       <div className='flex items-center whitespace-nowrap ease-out animate-autoscroll-x'>
-        <span className="text-xl text-gray-600 font-bold">Es noticia:</span>
+        <span className="text-sm text-gray-600 font-bold">Es noticia:</span>
         <ul className='inline-flex'>
           {separator_1_data.map((item, index) => (
             <li key={index}>
              {(index !== separator_1_data.length - 1) ? (
-                  <span className='text-gray-600 text-xl border-r-4 px-4 border-r-gray-200'>
+                  <span className='text-gray-600 text-sm border-r-4 px-4 border-r-gray-200'>
                     {item}
                   </span>
                 ) : (
-                  <span className='text-gray-600 text-xl px-4'>
+                  <span className='text-gray-600 text-sm px-4'>
                     {item}
                   </span>
                 )
@@ -25,7 +25,7 @@ function Separator1() {
           ))}
         </ul>
       </div>
-      <div className='absolute right-0 h-full w-1/12 bg-gradient-to-l from-white to-transparent'></div>
+      <div className='absolute right-0 h-full w-2/12 bg-gradient-to-l self-center from-gray-100 to-transparent'></div>
     </div>
   )
 }
@@ -48,7 +48,7 @@ function Separator2() {
   }, []);
   
   return (
-    <div className='container mx-auto border-y-4 border-y-gray-200 py-8 my-16 h-fit'>
+    <div className='container mx-auto border-y-4 border-y-gray-300 py-8 my-16 h-fit'>
       <img
         id='sticky-banner'
         className={`object-cover z-50 h-fit w-fit drop-shadow-xl mx-auto top-0 ${isSticky ? 'sticky' : 'static'}`}
@@ -62,7 +62,7 @@ function Separator2() {
 // Seccion scrolleable con cartas
 function Separator3() {
   return (
-    <div className='container mx-auto border-y-4 border-y-gray-200 py-8 my-16 overflow-hidden relative inline-flex'> 
+    <div className='container mx-auto border-y-4 border-y-gray-300 py-8 my-16 overflow-hidden relative inline-flex'> 
       <div className='flex overflow-x-auto'>
         <div className='grid grid-cols-3 gap-4 mx-auto flex'>
           {c_separator_data.map((card_data, index) => (
