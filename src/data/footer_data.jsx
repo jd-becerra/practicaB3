f4a428203bdc23065b1d803f6e8050a5b9366f0e
jd-becerra <jd-becerra>
2024-09-09 08:44:20 -0600

    Añadido footer

---
+++ src/data/footer_data.jsx
@@ -0,0 +1,35 @@
+const footer_links = [
+    {
+      title: 'Marca',
+      links: [
+        "App Marca", "Apuntes Deportivas", "Clasificación LaLiga", "Calendario LaLiga", "Hemeroteca Marca", "Marca eSpors", "Marca Madrid", "Marca en Orbyt", "Zona Marca"
+      ]
+    },
+    {
+      title: 'Ocio y Moda',
+      links: [
+        "Tiramillas", "Moda - Telva", "BUHO Magazine", "Traductor", "Programación y TV", "Marca Entradas", "Mi bebé y yo"
+      ]
+    },
+    {
+      title: 'Unidad Editorial',
+      links: [
+        "El Mundo", "Expansión", "CuidatePlus", "Diario Médico", "Correo Farmacéutico", "Subscripción Marca", "Subscripción El Mundo"
+      ]
+    },
+    {
+      title: 'Formación y Empleo',
+      links: [
+        "Escuela Unidad Editorial", "Unidad Editorial", "Expansión y Empleo"
+      ]
+    }
+]
+
+const footer_copyright = {
+  header: '© Unidad Editorial Información Deportiva, S.L.U. Todos los derechos reservados.',
+  links: [
+    'Política de cookies', 'Configuración de cookies', 'Política de privacidad', 'Términos y condiciones de uso', 'Certificado por OJD', 'Contacto', 'Publicidad'
+  ]
+}
+
+export { footer_links, footer_copyright }
